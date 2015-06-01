08f61e9cb6200e53f2e13dfed56e7ed5e47341d3
Sloop <liyulongss@qq.com>
2015-06-01 22:45:13 +0800

    示例程序
    更新示例程序

--- Sample/src/com/sloop/fontsmanager/demo/MainActivity.java
+++ Sample/src/com/sloop/fontsmanager/demo/MainActivity.java
@@ -1,35 +1,31 @@
 package com.sloop.fontsmanager.demo;
 
-import com.sloop.utils.fonts.FontsManager;
-
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.sloop.utils.fonts.FontsManager;
 
+/**
+ * @ClassName: MainActivity
+ * @author:	 	sloop
+ * @website:	http://www.sloop.icoc.cc
+ * @weibo: 		http://weibo.com/u/5459430586
+ * @date 2015年6月1日 下午10:41:54
+ */
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final ViewGroup root = (ViewGroup) findViewById(R.id.root);//找到View根节点
+		FontsManager.initFormAssets(this, "fonts/sao.ttf");
 		findViewById(R.id.button).setOnClickListener(new OnClickListener() {
 
+			@Override
 			public void onClick(View v){
-				//执行替换并获得替换结果
-				boolean isSuccess = FontsManager.changeFonts(MainActivity.this, root, "sao.ttf");
-				if (isSuccess) {
-					Toast.makeText(MainActivity.this, "替换成功", 1).show();
-				} else {
-					Toast.makeText(MainActivity.this, "替换失败", 1).show();
-				}
+				FontsManager.changeFonts(MainActivity.this);
 			}
 		});
 	}
