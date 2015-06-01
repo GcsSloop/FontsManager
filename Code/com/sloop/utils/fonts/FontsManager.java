01a8319821086e183738c91b992eed3aaa5ba7fa
Sloop <liyulongss@qq.com>
2015-06-01 22:16:54 +0800

    核心代码
    提交核心代码

--- Code/com/sloop/utils/fonts/FontsManager.java
+++ Code/com/sloop/utils/fonts/FontsManager.java
@@ -8,6 +8,7 @@
  */
 package com.sloop.utils.fonts;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.view.View;
@@ -27,35 +28,73 @@ import android.widget.TextView;
  */
 public class FontsManager {
 
+	private static Typeface defaultTypeface = null;
+
+	private FontsManager(){}
+
+	public static void init(Typeface typeface){
+		if (typeface == null) {
+			throw new IllegalStateException("typeface不能为空。");
+		} else {
+			defaultTypeface = typeface;
+		}
+	}
+
+	public static void initFormAssets(Context context, String fontPath){
+		try {
+			defaultTypeface = Typeface.createFromAsset(context.getAssets(), fontPath);
+		} catch (Exception e) {
+			throw new IllegalStateException("初始化失败，请检查fontsPath是否错误");
+		}
+	}
+
+	public static void changeFonts(Activity activity){
+		if (defaultTypeface == null) {
+			throw new IllegalStateException("必须先使用init()进行初始化");
+		}
+		changeFonts((ViewGroup) activity.findViewById(android.R.id.content), defaultTypeface);
+	}
+
 	/**
 	 * 更换字体
 	 * 
 	 * @Title: changeFonts
 	 * @param context 上下文
-	 * @param root view的父类
+	 * @param viewGroup view的父类
 	 * @param fonts 字体文件名称（例如：sloop.ttf 请注意，字体文件必须放在assets/fonts目录下）
-	 * @return 	true表示更换字体成功	false表示更换字体失败（可能是文件名称错误引起的）
+	 * @return true表示更换字体成功 false表示更换字体失败（可能是文件名称错误引起的）
 	 */
-	public static boolean changeFonts(Context context, ViewGroup root, String fonts){
+	public static void changeFonts(ViewGroup viewGroup, Typeface typeface){
 		try {
-			Typeface tf = Typeface.createFromAsset(context.getAssets(), "fonts/" + fonts);
-			for (int i = 0; i < root.getChildCount(); i++) {
-				View v = root.getChildAt(i);
-				if (v instanceof TextView) {
-					((TextView) v).setTypeface(tf);
-				} else if (v instanceof Button) {
-					((Button) v).setTypeface(tf);
-				} else if (v instanceof Switch) {
-					((Switch) v).setTypeface(tf);
-				} else if (v instanceof EditText) {
-					((EditText) v).setTypeface(tf);
-				} else if (v instanceof ViewGroup) {
-					changeFonts(context, (ViewGroup) v, fonts);
+			for (int i = 0; i < viewGroup.getChildCount(); i++) {
+				View v = viewGroup.getChildAt(i);
+				if (v instanceof ViewGroup) {
+					changeFonts((ViewGroup) v, typeface);
+				} else if (v instanceof View) {
+					changeFonts((View) v, typeface);
 				}
 			}
-			return true;
 		} catch (Exception e) {
-			return false;
+			// TODO
 		}
+	}
+
+	public static void changeFonts(View view, Typeface typeface){
+		try {
+			if (view instanceof ViewGroup) {
+				changeFonts((ViewGroup) view, typeface);
+			} else if (view instanceof TextView) {
+				((TextView) view).setTypeface(typeface);
+			} else if (view instanceof Button) {
+				((Button) view).setTypeface(typeface);
+			} else if (view instanceof Switch) {
+				((Switch) view).setTypeface(typeface);
+			} else if (view instanceof EditText) {
+				((EditText) view).setTypeface(typeface);
+			}
+		} catch (Exception e) {
+			// TODO
+		}
+
 	}
 }
